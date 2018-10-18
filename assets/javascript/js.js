090beb7e69f4c1b7d6da8a99a7abe0c89494300f
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-10-17 20:57:22 -0400

    much progress!

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -7,4 +7,19 @@ function myFunction() {
   }
 }
 
+$("#submit").click(function () {
+  var name = $("#name").val();
+  var subject = $("#subject").val();
+  var message = $("#message").val();
+  console.log(name, subject, message);
+
+  var mailTo = `mailto:amanda.elisabet.jordan@gmail.com?subject=${subject}&body=${message}
+
+  from: ${name}`
+
+  $("#submit").attr("href", mailTo);
+
+  console.log(mailTo);
+})
+
 
