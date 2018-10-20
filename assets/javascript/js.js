d0e77dc2018ba06106e5696c9660f4a2e01dfe92
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-10-20 13:54:49 -0400

    fixed the email form!

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,12 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("myTopNav");
-  if (x.className === "topnav") {
-      x.className += " responsive";
-  } else {
-      x.className = "topnav";
-  }
-}
-
 $("#submit").click(function () {
   var name = $("#name").val();
   var subject = $("#subject").val();
@@ -15,7 +6,7 @@ $("#submit").click(function () {
 
   var mailTo = `mailto:aj@ajjordan.co?subject=${subject}&body=${message}
 
-  from: ${name}`
+  from: ${name}`;
 
   $("#submit").attr("href", mailTo);
 
